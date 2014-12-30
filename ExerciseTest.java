0470626dc5397da2308ef9f42c91e5b8f7fada52
nimrahahmed <nimrah.ahmed2013@gmail.com>
2014-12-30 13:40:55 +0500

    Chapter5

--- ExerciseTest.java
+++ ExerciseTest.java
@@ -7,13 +7,22 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class ExerciseTest {
-	@Test public void testMultiplication() {
+	
+	public void testMultiplication() {
 		Dollar five= new Dollar(5);
 		assertEquals(new Dollar(10), five.times(2));
 		assertEquals(new Dollar(15), five.times(3));
-		}
-	 public void testEquality() {
+	}
+	 
+	public void testEquality() {
 		assertTrue(new Dollar(5).equals(new Dollar(5)));
 		assertFalse(new Dollar(5).equals(new Dollar(6)));
-		}
+	}
+
+	@Test public void testFrancMultiplication() {
+		Franc five= new Franc(5);
+		assertEquals(new Franc(10), five.times(2));
+		assertEquals(new Franc(15), five.times(3));
+	}
+
 }
