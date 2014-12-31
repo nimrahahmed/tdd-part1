25b3ef31dbae923a6ed0d598092cd424664454f7
nimrahahmed <nimrah.ahmed2013@gmail.com>
2014-12-31 12:43:30 +0500

    Chapter11

--- ExerciseTest.java
+++ ExerciseTest.java
@@ -14,23 +14,28 @@ public class ExerciseTest {
 		assertEquals(Money.dollar(15), five.times(3));
 	}
 
-	public void testEquality() {
+	/*public void testEquality() {
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
 		assertTrue(Money.franc(5).equals(Money.franc(5)));
 		assertFalse(Money.franc(5).equals(Money.franc(6)));
 		assertFalse(Money.franc(5).equals(Money.dollar(5)));
-	}
-	public void testFrancMultiplication() {
+	}*/
+	/*public void testFrancMultiplication() {
 		Money five = Money.franc(5); 
 		assertEquals(Money.franc(10), five.times(2));
 		assertEquals(Money.franc(15), five.times(3));
-	}
+	}*/
 	 public void testCurrency() {
 		assertEquals("USD", Money.dollar(1).currency());
 		assertEquals("CHF", Money.franc(1).currency());
 	}
-	 @Test public void testDifferentClassEquality() {
+	 @Test public void testEquality() {
+		 assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+		 assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+		 assertFalse(Money.franc(5).equals(Money.dollar(5)));
+		 }
+	 /*public void testDifferentClassEquality() {
 		assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
-		}
+		}*/
 }
--- Money.java
+++ Money.java
@@ -23,10 +23,10 @@ public class Money {
 		}
 
 	static Money dollar(int amount) {
-		return new Dollar(amount, "USD");
+		return new Money(amount, "USD");
 		} 
 	static Money franc(int amount) {
-		return new Franc(amount, "CHF");
+		return new Money(amount, "CHF");
 		}
 	public String toString() {
 		return amount + " " + currency;
