75236b228b172ffbedad085533cb2de069419cc7
nimrahahmed <nimrah.ahmed2013@gmail.com>
2014-12-31 10:34:12 +0500

    Chapter8

--- Franc.java
+++ Franc.java
@@ -4,8 +4,8 @@ public class Franc extends Money{
 	Franc(int amount) {
 	this.amount= amount;
 	}
-	Franc times(int multiplier) {
-	return new Franc(amount * multiplier);
+	Money times(int multiplier) {
+		return new Franc(amount * multiplier);
 	}
 	/*public boolean equals(Object object) {
 	Franc franc= (Franc) object;
