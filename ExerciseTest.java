48654bd0be556d766f042e7c983dfadd90c7bf99
nimrahahmed <nimrah.ahmed2013@gmail.com>
2015-01-01 12:38:29 +0500

    Chapter13

--- ExerciseTest.java
+++ ExerciseTest.java
@@ -31,6 +31,20 @@ public class ExerciseTest {
 		assertEquals("CHF", Money.franc(1).currency());
 	}
 	 
+	 @Test public void testPlusReturnsSum() {
+		 Money five= Money.dollar(5);
+		 Expression result= five.plus(five);
+		 Sum sum= (Sum) result;
+		 assertEquals(five, sum.augend);
+		 assertEquals(five, sum.addend);
+		 }
+		 
+	 @Test public void testReduceMoney() {
+		 Bank bank= new Bank();
+		 Money result= bank.reduce(Money.dollar(1), "USD");
+		 assertEquals(Money.dollar(1), result);
+		 }
+	 
 	 @Test public void testSimpleAddition() {
 		 Money five= Money.dollar(5);
 		 Expression sum= five.plus(five);
--- Money.java
+++ Money.java
@@ -37,7 +37,13 @@ public class Money implements Expression{
 	/*Money plus(Money addend) {
 		return new Money(amount + addend.amount, currency);
 		}*/
-	Expression plus(Money addend) {
+/*	Expression plus(Money addend) {
 		return new Money(amount + addend.amount, currency);
+		}*/
+	Expression plus(Money addend) {
+		return new Sum(this, addend);
+		}
+	public Money reduce(String to) {
+		return this;
 		}
 }
