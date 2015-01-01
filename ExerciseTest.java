699d18adfdfd9095528479bf98dea4ae3de51d1b
nimrahahmed <nimrah.ahmed2013@gmail.com>
2015-01-01 13:32:59 +0500

    Chapter16

--- ExerciseTest.java
+++ ExerciseTest.java
@@ -69,7 +69,29 @@ public class ExerciseTest {
 		 assertEquals(Money.dollar(1), result);
 		 }
 	 
-	 @Test public void testMixedAddition() {
+	 @Test public void testPlusSameCurrencyReturnsMoney() {
+		 Expression sum= Money.dollar(1).plus(Money.dollar(1));
+		 assertTrue(sum instanceof Money);
+		 }
+	 public void testSumTimes() {
+		 Expression fiveBucks= Money.dollar(5);
+		 Expression tenFrancs= Money.franc(10);
+		 Bank bank= new Bank();
+		 bank.addRate("CHF", "USD", 2);
+		 Expression sum= new Sum(fiveBucks, tenFrancs).times(2);
+		 Money result= bank.reduce(sum, "USD");
+		 assertEquals(Money.dollar(20), result);
+		 }
+	 public void testSumPlusMoney() {
+		 Expression fiveBucks= Money.dollar(5);
+		 Expression tenFrancs= Money.franc(10);
+		 Bank bank= new Bank();
+		 bank.addRate("CHF", "USD", 2);
+		 Expression sum= new Sum(fiveBucks, tenFrancs).plus(fiveBucks);
+		 Money result= bank.reduce(sum, "USD");
+		 assertEquals(Money.dollar(15), result);
+		 }
+	 public void testMixedAddition() {
 		 Expression fiveBucks= Money.dollar(5);
 		 Expression tenFrancs= Money.franc(10);
 		 Bank bank= new Bank();
--- Money.java
+++ Money.java
@@ -34,18 +34,18 @@ public class Money implements Expression{
 	/*Money times(int multiplier) {
 		return new Money(amount * multiplier, currency);
 		}*/
-	Expression times(int multiplier) {
+	/*Expression times(int multiplier) {
 		return new Money(amount * multiplier, currency);
-		}
+		}*/
 	/*Money plus(Money addend) {
 		return new Money(amount + addend.amount, currency);
 		}*/
 /*	Expression plus(Money addend) {
 		return new Money(amount + addend.amount, currency);
 		}*/
-	Expression plus(Money addend) {
+	/*Expression plus(Money addend) {
 		return new Sum(this, addend);
-		}
+		}*/
 	/*public Money reduce(String to) {
 		return this;
 		}*/
@@ -64,6 +64,10 @@ public class Money implements Expression{
 				}*/
 	public Expression plus(Expression addend) {
 		return new Sum(this, addend);
+		}
+	
+	public Expression times(int multiplier) {
+		return new Money(amount * multiplier, currency);
 		}
 	
 }
